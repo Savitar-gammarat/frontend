d8ee4b6f6627eb83503f88efce3d9e82cbbb88ba
Shawnewbslkk <1279363844@qq.com>
2019-01-06 01:53:50 +0800

    2019年1月6日01:53:45

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -5,8 +5,33 @@
 			<h3 class="md-title" style="flex: 1">
 				<logo></logo>Title
 			</h3>
-			<md-icon class="md-size-2x">view_headline</md-icon>
+			<md-icon style="cursor: pointer" class="md-size-2x" @click.native="showCard = !showCard">view_headline</md-icon>
 			<!--<slide-menu></slide-menu>-->
+			<div class="relative">
+				<md-card class="md-elevation-10 slide-card" v-if="!showCard">
+					<md-list>
+						<md-list-item class="toggle">
+							<md-icon>move_to_inbox</md-icon>
+							<span class="md-list-item-text">Rearrange</span>
+						</md-list-item>
+
+						<md-list-item class="toggle">
+							<md-icon>send</md-icon>
+							<span class="md-list-item-text">Preference</span>
+						</md-list-item>
+
+						<md-list-item class="toggle">
+							<md-icon>delete</md-icon>
+							<span class="md-list-item-text">Trash</span>
+						</md-list-item>
+
+						<md-list-item class="toggle">
+							<md-icon>error</md-icon>
+							<span class="md-list-item-text">Spam</span>
+						</md-list-item>
+					</md-list>
+				</md-card>
+			</div>
 		</md-toolbar>
 	</div>
 </template>
@@ -16,13 +41,31 @@
 	import Logo from "@/components/common/logo";
 	export default {
 		name: "navigation",
-		components: {Logo, SlideMenu}
+		components: {Logo, SlideMenu},
+		data(){
+			return{
+				showCard:false
+			}
+		}
 	}
 </script>
 
 <style scoped>
+.slide-card{
+	width: 300px;
+	position: absolute;
+	right: 20px;
+	top: 100px;
+
+}
 .navigation{
 	height: 150px;
 	box-shadow: none;
+}
+.md-list-item-text{
+	cursor: pointer;
+}
+.toggle :hover{
+	background-color: #eee;
 }
 </style>
